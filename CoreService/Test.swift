66f61b1947db2f828b3804b0494e3b5533b17805
Rubens G I Machion <rubens@helpie.com.br>
2019-10-21 11:19:26 -0300

    add test

---
+++ CoreService/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  CoreService
+//
+//  Created by Rubens Machion on 21/10/19.
+//  Copyright © 2019 Rubens Machion. All rights reserved.
+//
+
+import Foundation
+
+public class Teste: NSObject {
+    
+    public static func testePrint() {
+        print("Teste")
+    }
+}
