fc226a6112f1384e5c01e1701e05a377ccc3ddf8
Rubens G I Machion <rubens@helpie.com.br>
2019-10-21 11:09:28 -0300

    Firt commit

---
+++ CoreService/CoreService.h
@@ -0,0 +1,19 @@
+//
+//  CoreService.h
+//  CoreService
+//
+//  Created by Rubens Machion on 21/10/19.
+//  Copyright © 2019 Rubens Machion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreService.
+FOUNDATION_EXPORT double CoreServiceVersionNumber;
+
+//! Project version string for CoreService.
+FOUNDATION_EXPORT const unsigned char CoreServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreService/PublicHeader.h>
+
+
